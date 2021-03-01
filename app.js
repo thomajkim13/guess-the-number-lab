40687d6b95b27d6582642e9891472c654c51d7fd
thomasjkim13 <thomasjkim13@gmail.com>
2021-03-01 04:08:04 -0500

    add guess-the-number-lab

--- app.js
+++ app.js
@@ -7,15 +7,24 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+      do{
+        this.prevGuesses.push(this.getGuess())
+      }
+      while(this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)   
   },
+
   getGuess: function() {
     let guess;
     guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+    do {
+
+    }
     while(isNaN(guess) || guess<this.smallestNum || guess>this.biggestNum );
     return guess;
   },
   }
+
+  game.play()
  
   
 
-console.log(game.getGuess())
