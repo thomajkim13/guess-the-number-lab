f5f9ac55d5b725e8caa65e26697138130c877b04
thomasjkim13 <thomasjkim13@gmail.com>
2021-03-01 03:20:10 -0500

    add guess-the-number-lab

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const game = {
-    title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
-    secretNum: null,
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    }
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuesses: [],
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+  },
+  getGuess: function() {
+    let guess;
+    guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+    while(isNaN(guess) || guess<this.smallestNum || guess>this.biggestNum );
+    return guess;
+  },
   }
+ 
+  
+
+console.log(game.getGuess())
