c4f763b79eb70e2a64fd4f1e1af9e9d85eeaa46f
thomasjkim13 <thomasjkim13@gmail.com>
2021-03-01 04:59:27 -0500

    add new line

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-      do{
-        this.prevGuesses.push(this.getGuess())
-      }
-      while(this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)   
+    // Task number 4    
+   
   },
-
+  //Task number 2 and 3
   getGuess: function() {
     let guess;
-    guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
-    do {
-
-    }
+    guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));  
     while(isNaN(guess) || guess<this.smallestNum || guess>this.biggestNum );
     return guess;
   },
-  }
+  //Task number 5
 
-  game.play()
+
+  }
+  
+
+
  
   
 
